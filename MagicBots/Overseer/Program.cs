09bbd598afb4b0f091add7d7906937fcd5e0ad10
Harrison Lambeth <harrison@nau.edu>
2021-09-05 21:32:45 -0700

    Create MagicBots dotnet core solution

---
+++ MagicBots/Overseer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Overseer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
